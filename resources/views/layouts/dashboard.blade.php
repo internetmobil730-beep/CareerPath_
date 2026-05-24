7c553dd36ca33686c71984daadc1bd21fafce1a9
internetmobil730-beep <internetmobil730@gmail.com>
2026-05-24 22:20:51 +0300

    Add favicon logo to browser tab header

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="google" content="notranslate">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Kontrol Paneli')</title>
-
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
