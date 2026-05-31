3e4be3ec6636685933bad6a1494288d87a2dee07
internetmobil730-beep <internetmobil730@gmail.com>
2026-06-01 02:54:42 +0300

    favorite and search

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -2,33 +2,33 @@
 @section('title','Becerilerin Testi')
 @section('content')
 <div class="container">
-        <div class="row justify-content-center align-items-center mt-4">
-            <div class="col-12 col-md-8 col-lg-6">
-                <div class="d-flex align-items-center justify-content-center p-2 rounded shadow-sm">
+        <div class="row justify-content-center align-items-center mt-5 mb-4 px-3">
+    <div class="col-12 col-md-8 col-lg-6">
+        <div class="d-flex align-items-center justify-content-center p-2 rounded custom-search-container">
 
-                    <form action="{{ route('global.search') }}" method="GET" class="flex-grow-1 me-3">
-                        <div class="input-group">
-                            <input class="form-control" type="search" name="query"
-                                placeholder="Quiz, Bölüm veyaÜniversite arayın..." value="{{ request('query') }}"
-                                required>
-                            <button type="submit" class="btn mt-4">
-                                <i class="fa fa-search d-md-none"></i> <span class="search-btn d-none d-md-inline">araştırma</span>
-                            </button>
-                        </div>
-                    </form>
+            <form action="{{ route('global.search') }}" method="GET" class="flex-grow-1 me-2 mb-0">
+                <div class="input-group search-input-group">
+                    <input class="form-control search-field" type="search" name="query"
+                           placeholder="Quiz, Bölüm..." value="{{ request('query') }}" required>
+                    
+                    <button type="submit" class="btn search-submit-btn">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+            </form>
 
-                    <div class="header_icons">
-                        <div class="icon favorite-trigger">
-                            <a href="#">
-                                <i class="fa-regular fa-heart"></i>
-                                <span class="count count_favourite">0</span>
-                            </a>
-                        </div>
-                    </div>
-
+            <div class="header_icons flex-shrink-0">
+                <div class="icon favorite-trigger">
+                    <a href="#" class="d-block text-decoration-none">
+                        <i class="fa-regular fa-heart custom-heart-icon"></i>
+                        <span class="count count_favourite">0</span>
+                    </a>
                 </div>
             </div>
+
         </div>
+    </div>
+</div>
     <h3 class="title quiz-title mb-4">Yeteneklerinizi/İlgi alanlarınızı seçin</h3>
     <form method="POST" action="{{ route('quiz_results.submit') }}" id="quizForm">@csrf
         <div class="row">
