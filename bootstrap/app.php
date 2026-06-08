c5ec30eb34149d2a18111a85d9b6a8898bc7a7c0
internetmobil730-beep <internetmobil730@gmail.com>
2026-06-09 01:34:03 +0300

    Fix: hesap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'block.admin' => \App\Http\Middleware\BlockAdminFromWeb::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
